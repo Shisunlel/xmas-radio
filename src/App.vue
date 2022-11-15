ebc49cb5c98577d647030a83df1009586724c333
Daniel Kelly <me@danielkelly.io>
2022-11-14 22:26:15 -0600

    chore: cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { ref, watch, computed, nextTick } from 'vue'
 import { useMediaControls, useCycleList } from '@vueuse/core'
-import IconPlay from './components/icons/IconPlay.vue'
-import IconPlaying from './components/icons/IconPlaying.vue'
+import RadioSong from './components/RadioSong.vue'
 import RadioControls from './components/RadioControls.vue'
+import RadioHeader from './components/RadioHeader.vue'
 import KeyboardShortcuts from './components/KeyboardShortcuts.vue'
 
 const songs = [
@@ -32,24 +32,18 @@ const { playing, currentTime, duration, volume } = useMediaControls(audio, {
   src: current,
 })
 
-function playSong(song) {
-  state.value = song
-  playing.value = true
-}
-
-watch(current, () => {
+function playSong(song = null) {
+  if (song) state.value = song
   playing.value = false
   nextTick(() => {
-    console.log('play', current.value)
     playing.value = true
   })
-})
+}
+
+watch(current, () => playSong())
 
 watch(currentTime, () => {
-  if (currentTime.value === duration.value) {
-    next()
-    playSong(state.value)
-  }
+  if (currentTime.value === duration.value) next()
 })
 
 function isSelected(song) {
@@ -64,43 +58,17 @@ function isPlaying(song) {
     <div>
       <div class="shadow-md relative bg-white p-5 rounded-lg border-gray-100 border-[1px]">
         <KeyboardShortcuts />
-        <header class="text-center">
-          <h2 class="text-green text-2xl">
-            <span class="bg-gray-200 px-3.5 py-1 rounded text-sm font-bold">Advent of Vue</span>
-          </h2>
-          <h1 class="text-4xl mt-2 mb-5">Christmas Radio</h1>
-        </header>
-        <div class="flex">
-          <ul>
-            <li
-              v-for="song in songs"
-              @click="playSong(song)"
-              class="text-gray-500 cursor-pointer flex items-center mb-3 hover:bg-gray-200 pr-5"
-              :class="{
-                'bg-gray-200': isSelected(song),
-              }"
-            >
-              <div class="relative rounded overflow-hidden">
-                <div
-                  v-if="state.slug !== song.slug"
-                  class="hover:opacity-0 transition-opacity absolute top-0 right-0 left-0 bottom-0 bg-gray-900 opacity-50"
-                ></div>
-                <div
-                  class="pointer-events-none absolute text-white top-[50%] left-[50%]"
-                  style="transform: translate(-50%, -50%)"
-                >
-                  <IconPlaying v-if="isPlaying(song)" />
-                  <IconPlay v-else />
-                </div>
-
-                <img class="w-20" :src="`/img/${song.slug}.jpg`" :alt="song.title" />
-              </div>
-              <div class="ml-5">
-                <div>{{ song.title }}</div>
-              </div>
-            </li>
-          </ul>
-        </div>
+        <RadioHeader />
+        <ul>
+          <RadioSong
+            v-for="song in songs"
+            :key="song.slug"
+            :song="song"
+            :isSelected="isSelected(song)"
+            :isPlaying="isPlaying(song)"
+            @click="playSong(song)"
+          />
+        </ul>
         <RadioControls
           :progress="(currentTime / duration) * 100"
           @play="playing = true"
